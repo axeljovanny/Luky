98aeeb36860214b79fb691270dafc1b4236353d7
Axel <axel.jovanny.aj@gmail.com>
2026-04-08 15:39:35 -0600

    Agregamos Montserrat e hicimos un poco mas blanco la burbuja de texto

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'montserrat': ['Montserrat'],
+                'lato': ['Lato'],
+                'garamond': ['Garamond']
+            }
+        }
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/ui'),
+    ]
+}
